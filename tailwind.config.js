732278ede0b234aaf8d50478a45d1146bd7e1189
Suraj-bhan <surajbhanmundotiya@gmail.com>
2025-03-08 10:55:47 +0530

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        yellow: "#EB8400",
+        bg1: "#FFE6E5",
+      },
+      fontFamily: {
+        aurademo: ["aurademo", "sans-serif"], // after you font, add some fonts separated by commas to handle fallback.
+      },
+    },
+  },
+  plugins: [],
+};
